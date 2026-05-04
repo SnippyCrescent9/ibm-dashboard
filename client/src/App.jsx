c436bdd15a4a42970165ce2a76316eee9386eb85
Adam Sannes <adamsannes418@gmail.com>
2026-05-04 11:42:29 -0700

    testing connectio from frontend to backend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import {useEffect} from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
+  return <div>IBM Dashboard</div>;
+}
+
+export default App;
