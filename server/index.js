e66224745b483cfb61aee2ffc7215e72082d046c
Adam Sannes <adamsannes418@gmail.com>
2026-05-04 11:36:15 -0700

    set up index.js in server folder

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "IBM dashboard API running"});
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server rinning on port ${PORT}`);
+});
