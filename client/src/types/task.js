406fdd608e3d82ae7a6615cda675d24af89b0f81
Adam Sannes <adamsannes418@gmail.com>
2026-05-05 12:00:35 -0700

    created task.js type

---
+++ client/src/types/task.js
@@ -0,0 +1,18 @@
+/**
+ * @typedef {Object} Task
+ * @property {number} id
+ * @property {string} title
+ */
+
+/**
+ * Creates a new Task object
+ * @param {string} title
+ * @returns {Task}
+ */
+
+export function createTask(title) {
+    return {
+        id: Date.now(),
+        title
+    };
+}
