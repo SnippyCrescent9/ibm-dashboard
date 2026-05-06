b99aa966958bbad48701763158622c189a7b3540
Adam Sannes <adamsannes418@gmail.com>
2026-05-06 10:45:08 -0700

    added functionality to App.jsx and TaskList

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
 import {useState} from "react";
 import TaskForm from "./components/TaskForm";
-
+import TaskList from "./components/TaskList";
+import { createTask } from "./types/task";
 
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:5000/")
-      .then(res => res.json())
-      .then(data => console.log(data));
-  }, []);
+  const [tasks, setTasks] = useState([
+    createTask("Learn React basics"),
+    createTask("Build UI components")
+  ]);
 
-  return <div>IBM Dashboard</div>;
+  const addTask = (title) => {
+    const newTask = createTask(title);
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  return (
+    <div style={{padding: "20px"}}>
+      <h1>IBM Task Dashboard</h1>
+      <TaskForm onAdd={addTask}/>
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTask}
+      />
+    </div>
+  );
+
 }
 
 export default App;
