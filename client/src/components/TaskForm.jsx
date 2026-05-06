21f56d1ba0f25b96106a2cab83d6f94b8959c19f
Adam Sannes <adamsannes418@gmail.com>
2026-05-06 10:59:01 -0700

    added form to TaskForm

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -10,15 +10,19 @@ export default function TaskForm ({onAdd}) {
 
     return (
         <div style={{marginBottom: "20px"}}>
-            <input
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter") handleSubmit();
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
                 }}
-                placeholder="Enter a task!"
-            />
-            <button onClick={handleSubmit}>Add</button>
+            >
+                <input
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Enter a task!"
+                />
+                <button type="submit">Add</button>
+            </form>
         </div>
     );
 }
