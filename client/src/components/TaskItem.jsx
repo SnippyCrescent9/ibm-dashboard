ae44702c357e3ae58825ff424ae7d00f7e106408
Adam Sannes <adamsannes418@gmail.com>
2026-05-07 10:37:11 -0700

    added completion checkboxes to tasks in TaskItem.

--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -1,6 +1,18 @@
-export default function TaskItem({task, onDelete}) {
+export default function TaskItem({task, onDelete, onToggle}) {
     return (
-        <li style={{marginBottom: "10px"}}>
+        <li 
+            style={{
+                marginBottom: "10px",
+                textDecoration: task.completed
+                    ? "line-through"
+                    : "none"
+            }}
+        >
+            <input 
+                type="checkbox"
+                checked={task.completed}
+                onChange={()=> onToggle(task.id)}
+            />
             {task.title}
             <button onClick={() => onDelete(task.id)}>
                 Delete
